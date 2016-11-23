b09a8e8a755a10bc76f6ec9fc6dfd1eb4e5fd4e1
Michael Yabut <spanishiwa@gmail.com>
2016-11-22 17:20:50 -0800

    added constructor function which takes an options argument for MovingObject class

--- src/moving_object.js
+++ src/moving_object.js
@@ -1,1 +1,11 @@
-module.exports = MovingObject;
+export default class MovingObject {
+  constructor(options) {
+    this.pos = options.pos;
+    this.vel = options.vel;
+    this.radius = options.radius;
+    this.color = options.color;
+  }
+  draw() {
+    
+  }
+}
