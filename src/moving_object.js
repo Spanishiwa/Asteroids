fc6c5113a1b8b6880ff7a3fdf12f756809ffd008
Michael Yabut <spanishiwa@gmail.com>
2016-11-22 18:45:39 -0800

    added draw method to draw a circle for MovingObject class

--- src/moving_object.js
+++ src/moving_object.js
@@ -5,7 +5,13 @@ export default class MovingObject {
     this.radius = options.radius;
     this.color = options.color;
   }
-  draw() {
-    
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+
+    ctx.beginPath();
+    ctx.arc(
+      this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
+    );
+    ctx.fill();
   }
 }
