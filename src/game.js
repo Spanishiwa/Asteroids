605711cc8efc77884608369a6075bb7d7b3f5aac
Michael Yabut <spanishiwa@gmail.com>
2016-11-24 15:01:38 -0800

    Added wrap function for util and refactored util

--- src/game.js
+++ src/game.js
@@ -36,6 +36,7 @@ export default class Game {
       asteroid.move();
     });
   }
+
 }
 
 
--- src/util.js
+++ src/util.js
@@ -1,22 +1,32 @@
 let util = {
-  dist: function(pos1, pos2) {
+  dist(pos1, pos2) {
     let [x1, y1] = [pos1[0], pos1[1]];
     let [x2, y2] = [pos1[0], pos1[1]];
 
     return Math.sqrt((x2-x1)(x2-x1) + (y2-y1)(y2-y1));
   },
 
-  norm: function(vec) {
+  norm(vec) {
     return util.dist([0,0], vec);
   },
 
-  randVec: function(length) {
+  randVec(length) {
     let deg = 2 * Math.PI * Math.random();
     return util.scale([Math.sin(deg), Math.cos(deg)], length);
   },
 
-  scale: function(vec, m) {
+  scale(vec, m) {
     return [vec[0] * m, vec[1] * m];
+  },
+
+  wrap(coord, max) {
+    if (coord < 0) {
+      return max - (coord % max);
+    } else if (coord > max) {
+      return (coord % max);
+    } else {
+      return coord;
+    }
   }
 };
 
