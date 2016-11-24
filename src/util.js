c34b3ecf5b33fc8000d3afcd4be766a29d1063b1
Michael Yabut <spanishiwa@gmail.com>
2016-11-23 23:04:39 -0800

    removed es5 style inherits and added norm function to util

--- src/util.js
+++ src/util.js
@@ -6,8 +6,8 @@ let util = {
     return Math.sqrt((x2-x1)(x2-x1) + (y2-y1)(y2-y1));
   },
 
-  inherits: function(childClass, parentClass) {
-    
+  norm: function(vec) {
+    return util.dist([0,0], vec);
   }
 };
 
