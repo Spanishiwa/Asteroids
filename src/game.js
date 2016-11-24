60554cf818d7e6763f3a5d81d3d1d59f239e0a10
Michael Yabut <spanishiwa@gmail.com>
2016-11-24 13:50:42 -0800

    added constructor for game class and addAsteroids function

--- src/game.js
+++ src/game.js
@@ -8,7 +8,22 @@ let settings = {
 };
 
 export default class Game {
-  static run() {
-    console.log("Pretend I'm running the game now askdjfhaksldfh aklsjdfh!");
+  constructor() {
+    this.asteroids = [];
+  }
+
+  addAsteroids(NUM_ASTEROIDS) {
+    for(let i = 0; i < NUM_ASTEROIDS; i += 1) {
+      this.asteroids.push(new Asteroid);
+    }
   }
 }
+
+
+
+
+
+
+// static run() {
+//   console.log("Pretend I'm running the game now askdjfhaksldfh aklsjdfh!");
+// }
