bdfb8d505e42efcda100f44613d17c30da350b51
Michael Yabut <spanishiwa@gmail.com>
2016-11-24 14:19:33 -0800

    added draw method to Game which wipes the board, fills the board, and populates with Asteroids

--- src/asteroid.js
+++ src/asteroid.js
@@ -6,5 +6,7 @@ export default class Asteroid extends MovingObject {
     this.color = util.defaults.color;
     this.radius = util.defaults.radius;
     this.vel = options.vel || util.randVec(util.defaults.speed);
+
+    super(options);
   }
 }
--- src/game.js
+++ src/game.js
@@ -4,7 +4,8 @@ import { MovingObject } from './moving_object.js';
 let settings = {
   DIM_X: 1000,
   DIM_Y: 600,
-  NUM_ASTEROIDS: 10
+  NUM_ASTEROIDS: 10,
+  BG_COLOR: "302e2e"
 };
 
 export default class Game {
@@ -18,6 +19,16 @@ export default class Game {
     for(let i = 0; i < NUM_ASTEROIDS; i += 1) {
       this.asteroids.push(new Asteroid ({game: this}) );
     }
+  }
+
+  draw(ctx) {
+    ctx.clearRect(0, 0, settings.DIM_X, settings.DIM_Y);
+    ctx.fillStyle = settings.BG_COLOR;
+    ctx.fillRect(0, 0, settings.DIM_X, settings.DIM_Y);
+
+    this.asteroids.forEach(asteroid => {
+      asteroid.draw(ctx);
+    });
   }
 }
 
