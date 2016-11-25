6e30ec96019c20d33009999aab6aee85752df573
Michael Yabut <spanishiwa@gmail.com>
2016-11-25 11:44:38 -0800

    debugging asteroid constructor function

--- src/asteroid.js
+++ src/asteroid.js
@@ -1,9 +1,10 @@
 import { util, defaults } from './util.js';
 import { MovingObject } from './moving_object.js';
 
+
 export default class Asteroid extends MovingObject {
   constructor(options = {}) {
-    options.pos = options.pos;
+    options.pos = options.pos || options.game.randomPos();
     options.color = util.defaults.color;
     options.radius = util.defaults.radius;
     options.vel = options.vel || util.randVec(util.defaults.speed);
